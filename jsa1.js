5ebe51f8dcbaaf7ce2a64ae007da389034a80fa6
SANJAY U <112234870+Sanjayusa@users.noreply.github.com>
2025-04-03 20:37:20 +0530

    Add files via upload

---
+++ jsa1.js
@@ -0,0 +1,6 @@
+// Add animation class to login container on page load
+document.addEventListener("DOMContentLoaded", function () {
+    const loginContainer = document.querySelector(".login-container");
+    loginContainer.classList.add("animate"); // Apply animation class
+ });
+ 
